013032cbb85235b5293c9e5dbbc7373858d4c22d
hung <lexuanh121@gmail.com>
2022-01-05 23:50:41 +0700

    first commit

---
+++ js/btap3.js
@@ -0,0 +1,18 @@
+/**
+ * Khối 1 : input 
+ *soUsd
+ * 
+ * Khối 2 : 
+ * B1 : khai báo biến 
+ * B2 : 
+ */
+
+var usd =1;
+
+
+
+quyDoi= usd*23500
+
+
+console.log("so tien sau khi quy đổi là :"+quyDoi);
+
---
+++ js/btap4.js
@@ -0,0 +1,25 @@
+/**
+ * Khối 1 : Input
+ * chieuDai, chieuRong
+ * Khối 2 : 
+ * B1 : Khai báo 
+ * B2 : Tính chu vi 
+ * B3 : Tính diện tích 
+ * B4 : Hiển thị kết quả 
+ * 
+ * 
+ * Khối 3:
+ * Chu Vi , Dien Tich   
+ */
+
+var chieuDai=5;
+var chieuRong=6;
+
+chuVi= (chieuDai+chieuRong)*2;
+dienTich=chieuDai*chieuRong;
+
+
+console.log("chu vi hình chữ nhật :" + chuVi);
+console.log("dien tích hình chữ nhật :"+dienTich);
+
+
